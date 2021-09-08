6d47586bf49c40a49653a4468910c964f7083462
jfoutts-avenue8 <justin.foutts@avenue8.com>
2021-09-07 22:07:34 -0700

    Update typedefs

--- index.d.ts
+++ index.d.ts
@@ -4,20 +4,20 @@ export type Callback = (error?: Error) => void
 export type WritableCallback = (writable: Writable) => Promise<void>
 export type WritableStreamTreeFilter = (writable: WritableStreamTree) => Promise<boolean>
 
-export interface ReadableStreamTree {
-  finish(callback?: Callback): Readable
-  pipe(stream: Duplex): ReadableStreamTree
-  split(children?: number): ReadableStreamTree[]
-}
-
-export interface WritableStreamTree {
-  finish(callback?: Callback, pipe?: Readable): Writable
-  joinReadable(siblings: number): [WritableStreamTree, ReadableStreamTree[]]
-  joinWritable(siblings: Writable[], callback?: Callback[]): WritableStreamTree
-  pipeFrom(stream: Duplex): WritableStreamTree
-}
-
 declare namespace StreamTree {
+  export interface ReadableStreamTree {
+    finish(callback?: Callback): Readable
+    pipe(stream: Duplex): ReadableStreamTree
+    split(children?: number): ReadableStreamTree[]
+  }
+
+  export interface WritableStreamTree {
+    finish(callback?: Callback, pipe?: Readable): Writable
+    joinReadable(siblings: number): [WritableStreamTree, ReadableStreamTree[]]
+    joinWritable(siblings: Writable[], callback?: Callback[]): WritableStreamTree
+    pipeFrom(stream: Duplex): WritableStreamTree
+  }
+
   const readable: (stream: Readable) => ReadableStreamTree
   const writable: (stream: Writable) => WritableStreamTree
   const writer: (writeCallback: WritableCallback | WritableCallback[]) => WritableStreamTreeFilter
